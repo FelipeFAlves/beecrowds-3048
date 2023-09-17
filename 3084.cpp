41e7dd958cb77029602a1a3cd494024cb1ac6f37
FelipeFAlves <felipeferreira@asimovjr.com.br>
2023-09-17 11:26:47 -0300

    beecrowds-3048
    beecrowds-3048

---
+++ 3084.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Felipe Ferreira Alves - 2021020653
+int main(){
+
+    int a,b;
+
+    while (scanf("%d %d",&a,&b) != EOF){
+        printf("%02d:%02d\n",(a/30),(b/6));
+    }
+
+    return 0;
+}
